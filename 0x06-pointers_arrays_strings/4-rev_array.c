ebf556cfdd1627ea56bfc534dc02bedc043a12c8
onyishichetachi <victor.onyishi.pg02495@unn.edu.ng>
2022-12-23 13:48:01 -0800

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
  *  reverse_array - reverses the contents of an array
  *  @a: integer array to be reversed
  *  @n: length of array a
  *  Return: void
  */
+
 void reverse_array(int *a, int n)
 {
 	int i = 0 t;
