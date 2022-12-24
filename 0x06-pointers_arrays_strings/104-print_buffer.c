2b2125b7b03979bb0b891c28e6ac21249462bf7c
onyishichetachi <victor.onyishi.pg02495@unn.edu.ng>
2022-12-23 20:47:13 -0800

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,13 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *  * print_line - prints a s bytes of a buffer
- *   * @c: buffer to print
- *    * @s: bytes of buffer to print
- *     * @l: line of buffer to print
- *      *
- *       * Return: void
- *        */
+ *  print_line - prints a s bytes of a buffer
+ *  @c: buffer to print
+ *  @s: bytes of buffer to print
+ *  @l: line of buffer to print
+ *  Return: void
+ */
 
 void print_line(char *c, int s, int l)
 {
